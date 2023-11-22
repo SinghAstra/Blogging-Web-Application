58e495cd9618ba16e41b38b8867a284db5f2426c
Astra <abhaypratapsinghwd@gmail.com>
2023-11-22 17:41:46 +0530

    CRUD Functionality for Post

--- SnapBlog-Backend/controllers/postController.js
+++ SnapBlog-Backend/controllers/postController.js
@@ -2,6 +2,9 @@ const Post = require('../models/Post');
 
 module.exports.createPost = (req, res) => {
     const { title, content, image } = req.body;
+    if (!title || !content || !image) {
+        return res.json({ "message": "title, content and image are compulsory." })
+    }
     const post = new Post({ title, content, image });
     post.save().then(post => {
         res.json({ post })
@@ -10,14 +13,6 @@ module.exports.createPost = (req, res) => {
     })
 };
 
-module.exports.getAllPosts = async (req, res) => {
-    Post.find({}).then(posts => {
-        res.json({ posts })
-    }).catch(err => {
-        console.log(err);
-        res.json({ msg: "Error While Fetching Posts." })
-    })
-};
 
 module.exports.getPostById = (req, res) => {
     const postId = req.params.id;
@@ -33,4 +28,36 @@ module.exports.getPostById = (req, res) => {
             res.status(500).send('Error fetching post by ID');
         });
 };
+
+module.exports.updatePostById = (req, res) => {
+    const id = req.params.id;
+    const { title, content, image } = req.body;
+    if (!title || !content || !image) {
+        return res.json({ "message": "title, content and image are compulsory." })
+    }
+    Post.findByIdAndUpdate(id, { title, content, image })
+        .then(data => {
+            res.json({
+                message: "Post Updated Successfully.",
+                data
+            })
+        })
+        .catch(err => {
+            res.json({ err });
+        })
+}
+
+module.exports.deletePostById = (req,res) => {
+    const id = req.params.id;
+    Post.findByIdAndDelete(id)
+    .then(data=>{
+        res.json({
+            message:"Post Deleted Successfully",
+            data
+        })
+    })
+    .catch(err=>{
+        res.json({err})
+    })
+}
 
--- SnapBlog-Backend/routes/routes.js
+++ SnapBlog-Backend/routes/routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 
 router.post('/post', postController.createPost);
-router.get('/post', postController.getAllPosts);
 router.get('/post/:id',postController.getPostById);
+router.put('/post/:id',postController.updatePostById);
+router.delete('/post/:id',postController.deletePostById);
 
 module.exports = router;
