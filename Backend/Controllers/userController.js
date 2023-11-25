e5b3b049ed1685ea66d4ddf2544c17758184103a
Astra <abhaypratapsinghwd@gmail.com>
2023-11-25 21:32:08 +0530

    Implemeted Post at /editProfile

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require('../Models/user');
 const jwt = require('jsonwebtoken');
 
-const getProfile = async(req,res) => {
+const getProfile = async (req, res) => {
     const { JWT_SECRET_KEY } = process.env;
     const authorization = req.headers.authorization
     if (!(authorization && authorization.startsWith("Bearer"))) {
@@ -21,11 +21,45 @@ const getProfile = async(req,res) => {
     const user = await User.findById(decoded.id);
     req.user = user;
     return res.json({
-        success:true,
-        data:user
+        success: true,
+        data: user
+    })
+}
+
+const editProfile = async (req, res) => {
+    const { JWT_SECRET_KEY } = process.env;
+    const authorization = req.headers.authorization
+    if (!(authorization && authorization.startsWith("Bearer"))) {
+        return res.json({
+            "success": false,
+            "message": "You are not authorised to access this route."
+        })
+    }
+    const access_token = authorization.split(" ")[1]
+    const decoded = jwt.verify(access_token, JWT_SECRET_KEY);
+    if (!decoded) {
+        return res.json({
+            "success": false,
+            "message": "You are not authorised to access this route."
+        })
+    }
+    const user = await User.findById(decoded.id);
+    req.user = user;
+    const { email, username } = req.body
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, {
+        email, username
+    },
+        {
+            new: true,
+            runValidators: true
+        })
+    return res.status(200).json({
+        success: true,
+        data: updatedUser
     })
 }
 
 module.exports = {
-    getProfile
+    getProfile,
+    editProfile
 }
--- Backend/Routers/user.js
+++ Backend/Routers/user.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const userController = require('../Controllers/userController');
 
 router.get('/profile',userController.getProfile)
+router.post("/editProfile",userController.editProfile)
+// router.put("/changePassword",)
+// router.post("/:slug/addStoryToReadList",)
+// router.get("/readList",)
 
 
 module.exports = router
